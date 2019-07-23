4242b75213ff1c50c73b0346cb1d4cf74d494096
DarwinOS <darwinos@opensharing.fr>
2019-07-23 02:49:57 +0100

    UPDATE Buster 20190822

---
+++ UPDATE_Buster/Chamilo/scripts/debian-common.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt-get update
+apt-get upgrade -y
+
+FILE_HOSTS="/etc/hosts"
+
+grep -q "opensharing" "${FILE_HOSTS}"
+if [ $? != 0 ] ; then
+cat << EOF >> "${FILE_HOSTS}"
+127.0.0.1       localhost.localdomain                   localhost
+192.168.1.10    mylms.opensharing.priv                  mylms
+192.168.1.11    mydb.opensharing.priv                   mydb
+EOF
+fi
